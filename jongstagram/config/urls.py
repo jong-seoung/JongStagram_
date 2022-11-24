acb4362ca22fb9c6ee8ebb1d27af73cc0fc53a87
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-11-24 15:26:56 +0900

    [Django : Jongstagram] Title: 피드 생성하기 2
    서버로 파일 업로드 하는 API 생성

--- jongstagram/config/urls.py
+++ jongstagram/config/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from .views import main
 from polls.content.views import Main,UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Main.as_view()),
     path('content/upload',UploadFeed.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jongstagram/polls/content/views.py
+++ jongstagram/polls/content/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 # 같은 파일안에 .models라는 파일안에 Feed를 가지고옴
-from .models import Feed 
+from .models import Feed
+import os
+from uuid import uuid4 # 랜덤한 아이디를 넣어줌
+from config.settings import MEDIA_ROOT
 
 class Main(APIView):
     def get(self,request):
@@ -11,17 +14,25 @@ class Main(APIView):
 
 class UploadFeed(APIView):
     def post(self,request):
-        file = request.data.get('file')
-        image = request.data.get('image')
+
+        # 파일 불러오기
+        file = request.FILES['file']
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        # 파일을 읽어서 파일을 만들기
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = uuid_name
         content = request.data.get('content')
         user_id = request.data.get('user_id')
         profile_image = request.data.get('profile_image')
 
-        print(file)
-        print(image)
-        print(content)
-        print(user_id)
-        print(profile_image)
+        # 피드에 저장
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image,like_count =0)
 
 
         return Response(status=200)
