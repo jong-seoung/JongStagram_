96781abc26eef5cabe15be7b0a3f9af93515f4c7
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-11-29 16:40:57 +0900

    [Django : Jongstagram] Title : 좋아요, 북마크 기능 만들기, 내 프로필 페이지에 피드 필터 만들기
    인스타 클론 코딩 완성!!

--- jongstagram/polls/content/urls.py
+++ jongstagram/polls/content/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from polls.content.views import UploadFeed, Profile, UploadReply
+from polls.content.views import UploadFeed, Profile, UploadReply, ToggleLike, ToggleBookMark
 
 urlpatterns = [
     path('upload',UploadFeed.as_view()),
     path('profile',Profile.as_view()),
-    path('reply',UploadReply.as_view())
+    path('reply',UploadReply.as_view()),
+    path('like',ToggleLike.as_view()),
+    path('bookmark',ToggleBookMark.as_view())
 ]
